1cfc4b52b4174143ce9b930e6887d21e75b037c9
max van gorp <gorpmrh@ad.hva.nl>
2022-11-05 21:57:42 +0100

    added commands

--- music.py
+++ music.py
@@ -17,5 +17,33 @@ class Music(commands.Cog):
         else:
             await ctx.voice_client.move_top(voice_channel)
 
+    @commands.command()
+    async def disconnect(self, ctx):
+        await ctx.voice_client.disconnect()
+
+    @commands.command()
+    async def play(self, ctx, url):
+        ctx.voice_client.stop()
+        FFMPEG_OPTIONS = {'before_options': '-reconnect 1 -reconnect_streamed 1 -recpnnect_delay_max 5', 'options': '-vn'}
+        YDL_OPTIONS = {'format':'bestaudio'}
+        vc = ctx.voice_client
+
+        with youtube_dl.YoutubeDL(YDL_OPTIONS) as ydl:
+            info = ydl.extract_info(url, download=False)
+            url2 = info['formats'][0]['url']
+            source = await discord.FFmpegOpusAudio.from_probe(url2, **FFMPEG_OPTIONS)
+            vc.play(source)
+
+    @commands.command()
+    async def pause(self, ctx):
+        await ctx.voice_client.pause()
+        await ctx.send("Paused")
+
+    @commands.command()
+    async def resume(self, ctx):
+        await ctx.voice_client.resume()
+        await ctx.send("Resumed")
+
+
 def setup(client):
     client.add_cog(Music(client))
