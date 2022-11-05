dd1dd9c43593f033a4f4d3db8f9ade94ece0851a
max van gorp <gorpmrh@ad.hva.nl>
2022-11-05 20:56:54 +0100

    requirements and basic setup for the bot

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
-# This is a sample Python script.
+# to access the .env
+from decouple import config
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import discord
+from discord.ext import commands
 
+client = commands.Bot(command_prefix='?', intents= discord.Intents.all())
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+client.run(config('KEY'))
