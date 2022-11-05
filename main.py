a05cea32408f042af3eef2bec50d4cca3b207a5d
max van gorp <gorpmrh@ad.hva.nl>
2022-11-05 21:10:14 +0100

    rearrange main.py

--- main.py
+++ main.py
@@ -4,11 +4,12 @@ import discord
 from discord.ext import commands
 import music
 
+client = commands.Bot(command_prefix='?', intents= discord.Intents.all())
+
 cogs = [music]
 for i in range (len(cogs)):
-    cogs[i].setup()
+    cogs[i].setup(client)
 
 
-client = commands.Bot(command_prefix='?', intents= discord.Intents.all())
 
 client.run(config('KEY'))
