c1bf50df185dc44dee45dccb58284199389ec80e
max van gorp <gorpmrh@ad.hva.nl>
2022-11-06 14:29:31 +0100

    restart

--- main.py
+++ main.py
@@ -1,15 +1,13 @@
 import discord
-from discord.ext import commands
 import os
+import asyncio
+import youtube_dl
+import time
 
-from help_cog import help_cog
-from music_cog import music_cog
+# Discord bot Initialization
+client = discord.Client(command_prefix='!', intents=discord.Intents.all())
 
-bot = commands.Bot(command_prefix="/", intents=discord.Intents.all())
+key = os.getenv("DISCORD_TOKEN")
 
-bot.remove_command('help')
 
-bot.add_cog(help_cog(bot))
-bot.add_cog(music_cog(bot))
-
-bot.run("MTAzODU2MTUyOTI0MDQ4NTk0MA.G_1N7y.0Gu86JX6Egb5T-u07vHqtg-XCIhnS_fLxG5SAU")
+client.run(key)
